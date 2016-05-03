cdd4ba366cc677fe9d810325f34b12106cb1b7ca
Nicolas Jeuland <nicolas@jeuland.net>
2016-05-03 11:38:14 +0200

    initial commit

---
+++ src/User.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class User {
+	function var_dump_a($var) {
+		error_log(var_export($var, true));
+	}
+}
